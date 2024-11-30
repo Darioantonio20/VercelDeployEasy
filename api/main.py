190091851d0ce974eb4b8bc519cb016899c9c920
Darío Antonio Gutiérrez Álvarez <darioantoniogutierrezalvarez@MacBook-Air-2.local>
2024-11-30 13:41:17 -0600

    [Update and Fix]

--- api/main.py
+++ api/main.py
@@ -3,5 +3,5 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-async def health_check():
-    return "The health check is successful!"
+async def read_root():
+    return {"message": "Hello World"}
